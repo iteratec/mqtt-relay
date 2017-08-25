786751ea0cc50c44c21612df85ed8da006b5641e
Stefan Burnicki <stefan.burnicki@iteratec.de>
2017-08-25 12:36:14 +0200

    First implementation of MQTT relay

--- server.js
+++ server.js
@@ -1,14 +1,53 @@
-var mqtt = require('mqtt')
-var client  = mqtt.connect('mqtt://test.mosquitto.org')
+var mqtt = require('mqtt');
 
-client.on('connect', function () {
-  client.subscribe('presence')
-  client.publish('presence', 'Hello mqtt')
-})
+var srcHost = process.env.SOURCE;
+var destHost = process.env.DESTINATION;
+var topics = process.env.TOPICS;
 
-client.on('message', function (topic, message) {
-  // message is Buffer
-  console.log(message.toString())
-  client.end()
-})
+if (!srcHost) {
+  console.error("Please define a SOURCE host (as env variable)");
+  process.exit(1);
+}
+
+if (!destHost) {
+  console.error("Please define a DESTINATION host (as env variable)");
+  process.exit(1);
+}
+
+if (!topics) {
+  console.error("Please define TOPICS (as comma separated env variable) to forward");
+  process.exit(1);
+}
+topics = topics.split(",");
+
+
+var srcClient = mqtt.connect(srcHost);
+var destClient = mqtt.connect(destHost);
+
+srcClient.on('connect', function () {
+  console.log("Connected to source " + srcHost);
+  console.log("Subscribing to topics: " + topics.join(", "));
+  srcClient.subscribe(topics);
+});
+
+destClient.on('connect', function () {
+  console.log("Connected to destination " + destHost);
+});
+
+srcClient.on('error', function (error) {
+  console.error("Failed to connect to source " + srcHost, error);
+});
+
+destClient.on('error', function (error) {
+  console.error("Failed to connect to destination " + destHost, error);
+});
+
+srcClient.on('message', function (topic, message) {
+  if (!destClient.connected) {
+    console.log("Received message from source, but destination isn't connected. Dropping message");
+    return;
+  }
+  console.log("forwarding message on topic " + topic);
+  destClient.publish(topic, message);
+});
 
